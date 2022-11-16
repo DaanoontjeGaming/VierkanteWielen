49d383bccacf2eb3b1e05c6c85084f099a9381ba
Douwe <douweduyster@gmail.com>
2022-11-16 11:33:56 +0100

    Home aanpassingen

--- index.php
+++ index.php
@@ -9,57 +9,50 @@
 </head>
 <body>
     <div class="Navi">
+        <img src="Images/logo.jpg" class="nav-logo">
         <a href="home.html">Home</a>
         <a href="news.html">News</a>
         <a href="contact.html">Contact</a>
-        <a href="login.html">Log in</a>
+        <a href="inloggen.php">Log in</a>
     </div>
     <div class="imgcon">
         <img class="imgclass" src="Images/download.png">
         <div class="signup"><a href="registreren.php">Sign Up</a></div>
     </div>
     <div class="info">
-        <div>
-            <h1>Bladiebladie bla, mijn vader is een pa</h1>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
+        <div class="info-h1-p">
+            <h1>Rijschool vierkante Wielen helpt u op weg! TEST</h1>
+            <p> Rijschool vierkante Wielen bestaat al 19 jaar. Wij geven les in een ontspannen en vakkundige manier. zo hebben wij al meer dan 500 leerlingen geholpen met het behalen van hun rijbewijs. Waar wacht u nog op?</p>
         </div>
-        <img class="imgclass2" src="Images/rijles-instructeur-rijschool-auto-anp-1024.jpg" alt="">
+        <img class="geslaagd-img" src="Images/geslaagd.jpg" alt="">
     </div>
     <div class="reviews">
-        <img class="imgclass" src="/Images/download.png" alt="">
+        <img class="reviewimage" src="images/persoon1.png" alt="">
         <div>
-            <h1>Bladiebladie bla, mijn vader is een pa</h1>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
+            <!-- <h1>Bladiebladie bla, mijn vader is een pa</h1> -->
+            <p>Super! Goede en duidelijke uitleg, flexibel in afspraken maken en Sebastiaan maakt rijlessen nemen zelfs leuk en gezellig. Ik ga het zeker missen. Bedankt voor alles!</p>
         </div>
     </div>
     <div class="reviews2">
         <div>
-            <h1>Bladiebladie bla, mijn vader is een pa</h1>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
+            <h1></h1>
+            <p>"Je krijgt de ruimte om je eigen rijlessen in te plannen. Als je bijna gaat afrijden heb jij een streepje voor in de planning, en als je voor de zekerheid een extra lesje wilt dan is dat zo geregeld. Om het al een top rijschool, en buiten dat de auto heerlijk rijdt ga ik ook de gezelligheid missen van Sebastiaan!"</p>
         </div>
-        <img class="imgclass" src="/Images/download.png" alt="">
+        <img class="reviewimage" src="images/persoon2.png" alt="">
     </div>
     <div class="reviews">
-        <img class="imgclass" src="/Images/download.png" alt="">
+        <img class="reviewimage" src="images/persoon3.png" alt="">
         <div>
-            <h1>Bladiebladie bla, mijn vader is een pa</h1>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
+            <h1></h1>
+            <p>"ervaring: Erg tevreden. Flexibel in afspraken maken. Ook in het weekend en in de avond. Goed rijlessen te plannen naast een fulltime baan. Fijne instructeur die alle tijd neemt die nodig is en versnelt waar kan."</p>
         </div>
     </div>
     <div class="reviews2">
         <div>
-            <h1>Bladiebladie bla, mijn vader is een pa</h1>
-            <p>blablablablablablablablablablablablablablablablablabla. blablablablablablablablablablablabla? <br> blablablablablablablablablablablabla...<br><br></p>
+            <h1></h1>
+            <p>Dankzij de leuke en goede leerzame rijlessen in een keer geslaagd voor mijn praktijkexamen. Ook heb ik veel gehad aan de opnames die werden gemaakt tijdens de rijles.</p>
         </div>
-        <img class="imgclass" src="/Images/download.png" alt="">
+        <img class="reviewimage" src="images/persoon4.png" alt="">
     </div>
 </body>
 </html> 
