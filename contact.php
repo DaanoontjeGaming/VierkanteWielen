c30367bcbe24f1f0fb9fe8c53881248d0eedfdc4
Douwe <douweduyster@gmail.com>
2022-11-28 14:05:57 +0100

    contact styling fix

--- contact.php
+++ contact.php
@@ -8,37 +8,32 @@
     <link rel="stylesheet" type="text/css" href="contact.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
 </head>
-<!DOCTYPE html>
-<html lang="en">
-  <link rel="stylesheet" type="text/css" href="style.css">
 <body>
 <?php include('navbar.php')?>
 <div class="contact-container">
   <form action="contact-form-handler.php" method="post">
     <label for="name">Naam</label>
     <input type="text" id="name" name="name" placeholder="Uw naam..">
-  <div class="contact-container">
-    <form action="contact-form-handler.php" method="post">
-      <label for="name">Naam</label>
-      <input type="text" id="name" name="name" placeholder="Uw naam..">
 
-      <label for="email">Email</label>
-      <input type="text" id="email" name="email" placeholder="Uw email..">
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" placeholder="Uw email..">
 
-      <label for="subject">Onderwerp</label>
-      <input type="text" id="subject" name="subject" placeholder="Het onderwerp..">
+    <label for="subject">Onderwerp</label>
+    <input type="text" id="subject" name="subject" placeholder="Het onderwerp..">
   
-      <label for="message">Bericht</label>
-      <textarea id="message" name="message" placeholder="Type hier uw bericht.." style="height:200px"></textarea>
+    <label for="message">Bericht</label>
+    <textarea id="message" name="message" placeholder="Type hier uw bericht.." style="height:200px"></textarea>
 
-      <input type="submit" value="Submit">
+    <input type="submit" value="Submit">
 
   </form>
 </div>
 <?php include('footer.php')?>
 </body>
-</html>
-</html>
 
+
+
+
+
+</html>
