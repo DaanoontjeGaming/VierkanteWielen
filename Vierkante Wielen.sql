ce5272933b07edcb8af424e56f8fd8ae5940fd25
Daan Maat <dmaat@roc-dev.com>
2022-11-29 09:36:54 +0100

    Database edit

--- Vierkante Wielen.sql
+++ Vierkante Wielen.sql
@@ -5,6 +5,8 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Database VierkanteWielenDB
+-- -----------------------------------------------------
 CREATE DATABASE IF NOT EXISTS `VierkanteWielenDB` DEFAULT CHARACTER SET utf8 ;
 USE `VierkanteWielenDB` ;
 
@@ -22,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `Accounts` (
   UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE)
 ENGINE = InnoDB;
 
+INSERT INTO `Accounts` (`Username`, `Password`, `Email`, `Function`) VALUES ('JornOosterink', 'VierkanteRijschool', 'j.oosterink@rocvf.nl', 'Beheerder'), ('PietdeVries', 'VierkanteRijschool', 'p.devries@rocvf.nl', 'Beheerder');
 
 -- -----------------------------------------------------
 -- Table `Instructeurs`
@@ -50,8 +53,9 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `Lespaketten` (
   `PakketID` INT NOT NULL AUTO_INCREMENT,
   `Pakketnaam` VARCHAR(255) NOT NULL,
-  `Aantal uren` INT NOT NULL,
+  `Lesuren` INT NOT NULL,
   `Prijs` DECIMAL(6,2) NOT NULL,
+  `Soort auto` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`PakketID`))
 ENGINE = InnoDB;
 
