339ddc4860d4434435d20d32e4d361fd021e4057
Daan Maat <dmaat@roc-dev.com>
2022-12-01 13:56:48 +0100

    SQL File update

--- Vierkante Wielen.sql
+++ Vierkante Wielen.sql
@@ -4,6 +4,11 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Database VierkanteWielenDB
+-- -----------------------------------------------------
+CREATE DATABASE IF NOT EXISTS `VierkanteWielenDB` DEFAULT CHARACTER SET utf8 ;
+USE `VierkanteWielenDB` ;
 
 -- -----------------------------------------------------
 -- Table `Accounts`
@@ -15,8 +20,8 @@ CREATE TABLE IF NOT EXISTS `Accounts` (
   `Email` VARCHAR(45) NOT NULL,
   `Function` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`AccountID`),
-  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC),
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC))
+  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE,
+  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 INSERT INTO `Accounts` (`Username`, `Password`, `Email`, `Function`) VALUES ('JornOosterink', 'VierkanteRijschool', 'j.oosterink@rocvf.nl', 'Beheerder'), ('PietdeVries', 'VierkanteRijschool', 'p.devries@rocvf.nl', 'Beheerder');
@@ -33,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `Instructeurs` (
   `Telefoon` VARCHAR(10) NOT NULL,
   `AccountID` INT NOT NULL,
   PRIMARY KEY (`InstructeursID`),
-  INDEX `fk_Instructeurs_Accounts_idx` (`AccountID` ASC),
+  INDEX `fk_Instructeurs_Accounts_idx` (`AccountID` ASC) VISIBLE,
   CONSTRAINT `fk_Instructeurs_Accounts`
     FOREIGN KEY (`AccountID`)
     REFERENCES `Accounts` (`AccountID`)
@@ -55,8 +60,8 @@ CREATE TABLE IF NOT EXISTS `Lespaketten` (
 ENGINE = InnoDB;
 
 INSERT INTO `Lespaketten` (`Pakketnaam`, `Prijs`, `Autosoort`, `Lesuren`)
-VALUES ('Schakel 36', '2199', 'Schakelauto', '36'), ('Schakel 40', '2499', 'Schakelauto', '40'), ('Schakel 44', '2799', 'Schakelauto', '44'), ('Auto 36', '2399', 'Automaat', '36'),
-('Auto 40', '2699', 'Automaat', '40'), ('Auto 44', '2999', 'Automaat', '44'), ('Compleet 36', '2999', 'Beide', '36'), ('Compleet 40', '3399', 'Beide', '40'), ('Compleet 44', '3799', 'Beide', '44');
+VALUES ('Schakel 36', '2199.99', 'Schakelauto', '36'), ('Schakel 40', '2499.99', 'Schakelauto', '40'), ('Schakel 44', '2799.99', 'Schakelauto', '44'), ('Auto 36', '2399.99', 'Automaat', '36'),
+('Auto 40', '2699.99', 'Automaat', '40'), ('Auto 44', '2999.99', 'Automaat', '44'), ('Compleet 36', '2999.99', 'Beide', '36'), ('Compleet 40', '3399.99', 'Beide', '40'), ('Compleet 44', '3799.99', 'Beide', '44'), ('Losse les', '59.99', 'Beide', '1');
 
 -- -----------------------------------------------------
 -- Table `Leerlingen`
@@ -71,8 +76,8 @@ CREATE TABLE IF NOT EXISTS `Leerlingen` (
   `AccountID` INT NOT NULL,
   `PakketID` INT NOT NULL,
   PRIMARY KEY (`LeerlingID`),
-  INDEX `fk_Instructeurs_Accounts_idx` (`AccountID` ASC),
-  INDEX `fk_Leerlingen_Lespaketten1_idx` (`PakketID` ASC),
+  INDEX `fk_Instructeurs_Accounts_idx` (`AccountID` ASC) VISIBLE,
+  INDEX `fk_Leerlingen_Lespaketten1_idx` (`PakketID` ASC) VISIBLE,
   CONSTRAINT `fk_Instructeurs_Accounts0`
     FOREIGN KEY (`AccountID`)
     REFERENCES `Accounts` (`AccountID`)
@@ -94,8 +99,8 @@ CREATE TABLE IF NOT EXISTS `Lessen` (
   `Timeslot` DATETIME NOT NULL,
   `LeerlingID` INT NOT NULL,
   `InstructeursID` INT NOT NULL,
-  INDEX `fk_Leerlingen_has_Instructeurs_Instructeurs1_idx` (`InstructeursID` ASC),
-  INDEX `fk_Leerlingen_has_Instructeurs_Leerlingen1_idx` (`LeerlingID` ASC),
+  INDEX `fk_Leerlingen_has_Instructeurs_Instructeurs1_idx` (`InstructeursID` ASC) VISIBLE,
+  INDEX `fk_Leerlingen_has_Instructeurs_Leerlingen1_idx` (`LeerlingID` ASC) VISIBLE,
   PRIMARY KEY (`LesID`),
   CONSTRAINT `fk_Leerlingen_has_Instructeurs_Leerlingen1`
     FOREIGN KEY (`LeerlingID`)
@@ -109,6 +114,7 @@ CREATE TABLE IF NOT EXISTS `Lessen` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+SELECT * FROM Lespaketten;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
