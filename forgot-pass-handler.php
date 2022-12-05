2363433d48d00f1c26a70cce90d4d0b7759afc9e
ddev01 <douweduyster@gmail.com>
2022-12-05 02:17:00 +0100

    Lokale ww vergeten token generatie

---
+++ forgot-pass-handler.php
@@ -0,0 +1,51 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+$code = rand(1000,9999);
+$to = $_POST['email']; //Naar wie de contact form content word gestuurd (rijschoolhouder vierkantewielen@gmail.com)
+$servermail = ''; //Email die gebruikt word om mails te versturen
+$servermailpass = ''; //Email app wachtwoord (anders als normaal WW zie https://myaccount.google.com/u/0/apppasswords)
+
+$db = new PDO('mysql:127.0.0.1;dbname=resetpw', 'root', '');
+$result = $db->query("SELECT * from resetpw.resetpwtable WHERE email='$to'");
+if ($result->rowCount()>0) {
+  $db->query("UPDATE resetpw.resetpwtable SET reset_code='$code' WHERE email='$to'");
+  echo "exists";
+  }
+  else {
+    echo "doesnt exist";
+    $db->query("INSERT INTO resetpw.resetpwtable (`email`, `reset_code`) VALUES ('$to', '$code')");
+  }
+
+// $db->query("UPDATE resetpw.resetpwtable SET reset_code='$code' WHERE email='$to'");
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = $servermail;
+    $mail->Password   = $servermailpass;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;                                
+    //Recipients
+    $mail->setFrom('vierkantewielen@gmail.com', 'Administrator');
+    $mail->addAddress($to);
+
+    //Content
+    $mail->isHTML(true);    
+    $mail->Subject = 'Wachtwoord vergeten';
+    $mail->Body = 'Vul deze code in om uw wachtwoord te resetten:'. $code;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Reset code has been sent';
+    // header('Location: index.php');
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- forgot-pass.php
+++ forgot-pass.php
@@ -1,16 +1,23 @@
-<?php
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vierkante Wielen contact</title>
+    <link rel="stylesheet" type="text/css" href="contact.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    
+<?php include('navbar.php')?>
+<div class="contact-container">
+    <form action="forgot-pass-handler.php" method="post">
+        <label for="email">Voer uw email adres in en wij zullen u een code sturen om uw wachtwoord te resetten</label>
+        <input type="text" id="email" name="email" placeholder="Uw email.." style="margin-top:20px">
+        <input type="submit" value="Submit">
+  </form>
+</div>
+<?php include('footer.php')?>
 </body>
 </html>
