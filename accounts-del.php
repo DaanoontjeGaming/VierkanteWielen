acffaf0b9987a9c7c556645dbb171fcfa2369bb0
Douwe <douweduyster@gmail.com>
2022-12-14 13:11:10 +0100

    restore

---
+++ accounts-del.php
@@ -0,0 +1,6 @@
+<?php
+	$id=$_GET['id'];
+  $db = new PDO("mysql:host=localhost;dbname=VierkanteWielenDB", 'root', '');
+  $db->query("delete from `Accounts` where AccountID='$id'");
+	header('location:accounts-table.php');
+?>
