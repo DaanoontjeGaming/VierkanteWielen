8d2dfa440cf91c9c72518403bf14f8d07903e3c4
Rianne Mara van Leeuwen <riannemaravanl@gmail.com>
2023-01-09 13:10:32 +0100

    leerlingen en medewerkers pagina html af. php nog toevoegen!!!!

---
+++ leerlingen.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alle Leerlingen</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php include('navbar.php') ?>
+
+    <div class="welcome">
+        <h1>Bekijk leerlingen</h1>
+    </div>
+
+    <div class="roundRect" style="background-color:#E6E6E6">
+        <table class="dashboardTable">
+            <tr>
+                <th>Naam</th>
+                <th>Klantnummer</th>
+                <th>Pakket</th>
+                <th>Lessen</th>
+                <th>Betaald</th>
+            </tr>
+            <!-- Dit is alleen een voorbeeld. 
+                De volgende tabelrijen moeten uit de database komen. -->
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(aantal lessen)</td>
+                <td>(bedrag betaald)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(26/40)</td>
+                <td>(€400/€1000)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(16/25)</td>
+                <td>(€250/€800)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(11/40)</td>
+                <td>(€1400/€1400)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(26/40)</td>
+                <td>(€400/€1000)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(16/25)</td>
+                <td>(€250/€800)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(11/40)</td>
+                <td>(€1400/€1400)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(26/40)</td>
+                <td>(€400/€1000)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(16/25)</td>
+                <td>(€250/€800)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(11/40)</td>
+                <td>(€1400/€1400)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(26/40)</td>
+                <td>(€400/€1000)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(16/25)</td>
+                <td>(€250/€800)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(11/40)</td>
+                <td>(€1400/€1400)</td>
+            </tr>
+            <tr class="rowLeerling">
+                <td>(volledige naam)</td>
+                <td>(klantnummer)</td>
+                <td>(pakket naam)</td>
+                <td>(11/40)</td>
+                <td>(€1400/€1400)</td>
+            </tr>
+        </table>
+    </div>
+
+    <?php include('footer.php') ?>
+</body>
+
+</html>
---
+++ medewerkers.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alle medewerkers</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <!-- <?php include('navbar.php') ?> -->
+
+    <div class="welcome">
+        <h1>Bekijk medewerkers</h1>
+    </div>
+
+    <div class="roundRect" style="background-color:#E6E6E6">
+        <table class="dashboardTable">
+            <tr>
+                <th>Naam</th>
+                <th>Medewerkernummer</th>
+                <th>Uren</th>
+                <th>Leelingen</th>
+                <th>Salaris maandelijks</th>
+            </tr>
+            <!-- Dit is alleen een voorbeeld. 
+                De volgende tabelrijen moeten uit de database komen. -->
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(uren i/d week)</td>
+                <td>(aantal leerlingen)</td>
+                <td>(salaris)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+            <tr class="rowMedewerker">
+                <td>(volledige naam)</td>
+                <td>(Medewerkernummer)</td>
+                <td>(32)</td>
+                <td>(15)</td>
+                <td>(€2000,60)</td>
+            </tr>
+        </table>
+
+
+
+    </div>
+    <div class="roundRect toevoegButtonDiv" style="background-color:#E6E6E6">
+        <a href="accounts-table.php">
+            <button class="buttonBlue toevoegButton" style="color: white;">Voeg medewerker toe</button>
+        </a>
+    </div>
+
+    <!-- <?php include('footer.php') ?> -->
+</body>
+
+</html>
