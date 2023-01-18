cdbaf388828f6f9c8ec40121ddec94dd2845fa87
Juustie <jhillecamp@roc-dev.com>
2023-01-18 09:55:43 +0100

    Responsive + code fix

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function showSlides() {
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "flex";
   setTimeout(showSlides, 3600); 
 }
