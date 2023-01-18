9262d51b83da7d93ff7d8ed6455e11f00a5b5c5c
Daan Maat <dmaat@roc-dev.com>
2023-01-18 12:13:11 +0100

    File fix

--- confirm.php
+++ confirm.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
-require_once('functions.php');
 
 $username = $_POST['Username'];
 $password = $_POST['Password'];
 
     $user = 'root';
     $pass = 'root';
-    $db = new PDO("mysql:host=vierkantewielen_db_1;dbname=VierkantieWielenDB", $user, $pass);
+    $db = new PDO("mysql:host=vierkantewielen_db_1;dbname=VierkanteWielenDB", $user, $pass);
 $stmt = $db->prepare("SELECT * FROM Accounts WHERE Username = '$username'");
 $stmt->execute(['username' => $username]); 
 $result = $stmt->fetchAll(); 
@@ -15,10 +14,10 @@ if(count($result) === 1){
     $_SESSION['Username'] = $username;
     $_SESSION['Ingelogd'] = true;
     $_SESSION['false-user-pass'] = "";
-    header("location: index.php");
+    header('location: index.php');
 }else{
     $_SESSION['Ingelogd'] = false;
     $_SESSION['false-user-pass'] = "<p class='red melding'>Onjuiste gebruikersnaam of wachtwoord!<p>";
-    header("location: inloggen.php");
+    header('location: inloggen.php');
 };
 ?>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,5 @@
-<div class="Navi">
+  <div class="Navi">
+  <img href="index.php" src="images/logo.jpg">
   <a href="index.php">Home</a>
   <a href="news.html">News</a>
   <a href="contact.php">Contact</a>
@@ -12,17 +13,26 @@
    $string = $result[0];
    $accountID = intval($string);
    $Functie = $db->query("SELECT `Function` FROM Accounts WHERE AccountID = $accountID");
+   $Vname = $db->query("SELECT `Voornaam` FROM Leerlingen WHERE AccountID = '$accountID'")->fetch();
+   $Voornaam = $Vname[0];
+   $Tv = $db->query("SELECT `Voornaam` FROM Leerlingen WHERE AccountID = '$accountID'")->fetch();
+   $Tussenvoegsel = $Tv[0];
+   $Aname = $db->query("SELECT `Achternaam` FROM Leerlingen WHERE AccountID = '$accountID'")->fetch();
+   $Achternaam = $Aname[0];
+   $namelink = $Voonaam.$Tussenvoegsel.$Achternaam;
+   $link = '<a target="_blank" href="https://calendly.com/'.$namelink.'/rijles/">Rijles plannen</a>';
   if(!$_SESSION['Ingelogd']){
     echo '<a href="inloggen.php">Log in</a>';
   } else {
     if($Functie === 'Beheerder'){
-    echo '<a href="">Profiel</a>'; //Rijschoolhouder profiel pagina is nog niet 100% af
+    echo '<a href="profiel_rijschoolhouder.php">Profiel</a>'; 
   } else if ($Functie === 'Instructeur') {
     echo '<a href="Profiel_Instructeur.php">Profiel</a>';
   } else {
-    echo '<a href="">Profiel</a>'; //Leerling profiel pagina is nog niet 100% af
+    echo $link;
+    echo '<a href="profiel_leerling.php">Profiel</a>'; 
 }
-  echo '<a href="inloggen.php?loguit">uitloggen</a>';
+  echo '<a href="inloggen.php?loguit">Uitloggen</a>';
 }
   ?>
 </div>
